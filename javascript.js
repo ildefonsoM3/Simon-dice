dd0c7751e15627a91ac884d61e850a346c982b86
Ildefonso Muro <ildefonso.muro@gmail.com>
2020-11-08 22:52:00 -0600

    numero random

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const  btnEmpezar = document.getElementById('btnEmpezar');
+const btnEmpezar = document.getElementById('btnEmpezar');
 const celeste = document.getElementById('celeste');
 const violeta = document.getElementById('violeta');
 const naranja = document.getElementById('naranja');
@@ -6,11 +6,23 @@ const verde = document.getElementById('verde');
 
 class Juego {
     constructor() {
-        this.inicializar()
+        this.inicializar();
+        this.generarSecuencia();
     }
 
     inicializar() {
         btnEmpezar.classList.add("hide");
+        this.nivel = 1;
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    generarSecuencia() {
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
     }
 }
 
