ba2a5e499a7e598753913043f83102af6120692e
Ildefonso Muro <ildefonso.muro@gmail.com>
2020-11-15 23:06:58 -0600

    Iluminacion de colores

--- javascript.js
+++ javascript.js
@@ -8,21 +8,51 @@ class Juego {
     constructor() {
         this.inicializar();
         this.generarSecuencia();
+        this.siguienteNivel();
     }
 
     inicializar() {
         btnEmpezar.classList.add("hide");
-        this.nivel = 1;
+        this.nivel = 6;
         this.colores = {
-            celeste,
-            violeta,
-            naranja,
-            verde
+            celeste: celeste,
+            violeta: violeta,
+            naranja: naranja,
+            verde: verde
         }
     }
 
     generarSecuencia() {
         this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+
+    siguienteNivel() {
+        this.iluminarSecuencia();
+    }
+
+    transformarNumeroAColor(numero) {
+        switch (numero) {
+            case 0:
+                return 'celeste';
+            case 1:
+                return 'violeta';
+            case 2:
+                return 'naranja';
+            case 3:
+                return 'verde';
+        }
+    }
+
+    iluminarSecuencia() {
+        for (let i = 0; i < this.nivel; i++) {
+            const color = this.transformarNumeroAColor(this.secuencia[i]);
+            setTimeout(() => this.iluminarColor(color), 1300 * i);
+        }
+    }
+
+    iluminarColor(color) {
+        this.colores[color].classList.add('light');
+        setTimeout(() => this.colores[color].classList.remove('light'), 350);
     }
 }
 
